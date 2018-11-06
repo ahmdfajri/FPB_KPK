40064f53e65caa044e1c30e3c4318aff73799596
ahmdfajri <34332107+ahmdfajri@users.noreply.github.com>
2018-11-06 11:09:29 +0700

    Add files via upload

---
+++ SoalFPB_KPK.js
@@ -0,0 +1,18 @@
+  function kpk(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+     return false;
+   return (!x || !y) ? 0 : Math.abs((x * y) / fpb(x, y));
+ }
+ 
+ function fpb(x, y) {
+   x = Math.abs(x);
+   y = Math.abs(y);
+   while(y) {
+     var t = y;
+     y = x % y;
+     x = t;
+   }
+   return x;
+ }
+ console.log(fpb(3,2))
+ console.log(kpk(3,2))
